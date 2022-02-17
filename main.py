ab698bcadcc75f11dbdda7e005107627e7a0811a
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2022-02-18 00:29:06 +0300

    Переименовал переменные, указывающие на путь к файлам, из которых берем оригинальный язык, предыдущую версию перевода и добавил путь к новому файлу, в который записывается компиляция из уже существующего перевода и новых строчек на оригинале. Добавил вывод в файл с пометкой всех непереведенных строчек.

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-from config import default_language_path, previous_translate_path
+from config import default_language_path, previous_translate_path, new_translate_path
 
-with open(file=default_language_path, mode="r",
-          encoding="utf-8-sig") as new_language, \
-        open(file=previous_translate_path, mode="r",
-             encoding="utf-8-sig") as default_language:
-
+with open(file=previous_translate_path, mode="r",
+          encoding="utf-8-sig") as previous_translate, \
+        open(file=default_language_path, mode="r",
+             encoding="utf-8-sig") as default_language, \
+        open(file=new_translate_path, mode="w", encoding="utf-8-sig") as new_translate:
     default_language_dictionary = {}
     default_language_lines = default_language.readlines()
 
-    new_language_list = ["" for _ in range(len(default_language_lines))]
-    new_language_dictionary = {"lang": new_language.readline()}
-    new_language_lines = new_language.readlines()
+    new_translate_list = ["" for _ in range(len(default_language_lines))]
+    previous_translate_dictionary = {"lang": previous_translate.readline()}
+    previous_translate_lines = previous_translate.readlines()
 
-    for line in new_language_lines:
-        ls = line.lstrip()
+    for line in previous_translate_lines:
         if line.lstrip() != "":
-            new_language_dictionary[line.split()[0]] = line
+            previous_translate_dictionary[line.split()[0]] = line
 
     num_str = 0
     for line in default_language_lines:
-        if line.replace(" ", "") != "\n":
+        if line.lstrip() != "":
             key = line.split()[0]
             value = line.lstrip()
         else:
@@ -29,10 +28,13 @@ with open(file=default_language_path, mode="r",
         num_str += 1
     for key, values in default_language_dictionary.items():
         if key == 0:
-            new_language_list[0] = new_language_dictionary["lang"]
+            new_translate_list[0] = previous_translate_dictionary["lang"]
         else:
-            response = new_language_dictionary.get(values["key"], None)
-            if response is None:
-                new_language_list[key] = values["value"]
+            response = previous_translate_dictionary.get(values["key"], None)
+            if response is None and values["key"] != "transfer":
+                new_translate_list[key] = "#NT! " + values["value"]
+            elif values["key"] == "transfer":
+                new_translate_list[key] = values["value"]
             else:
-                new_language_list[key] = new_language_dictionary[values["key"]]
+                new_translate_list[key] = previous_translate_dictionary[values["key"]]
+    print(*new_translate_list, end="", sep="", file=new_translate)
