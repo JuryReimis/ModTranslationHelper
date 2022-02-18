4123c4d6f2b798f2627b578c3686ef88460bb068
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2022-02-18 19:08:00 +0300

    В get_original_localization_hierarchy иерархия предпологает, что она уже находится в папке с названием языка, то есть из пути каждого файла убраны названия языка. В make_new_language_hierarchy добавлена проверка, является ли путь директорией. make_original_language_dictionary перенесен выше без изменений
    Добавлен цикл перебора по значениям иерархии файлов, каждый файл открывается и в него вносятся изменения

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def need_translation():
 def get_original_localization_hierarchy(original_path: str) -> list:
     hierarchy = []
     for step in os.walk(original_path):
-        language_path_divided = os.path.relpath(step[0], start=os.path.dirname(original_path))
+        language_path_divided = os.path.relpath(step[0], start=original_path)
         for name in step[2]:
             hierarchy.append(os.path.join(language_path_divided, name))
     return hierarchy
@@ -42,9 +42,24 @@ def make_new_language_hierarchy(new_translation_path, target_language, original_
     for file in original_hierarchy:
         target_file = file.replace(original_language, target_language)
         target_path = os.path.join(new_translation_path, target_file)
-        if not os.path.exists(os.path.dirname(target_path)):
+        if os.path.isdir(target_path) and not os.path.exists(os.path.dirname(target_path)):
             os.makedirs(os.path.dirname(target_path))
             print(f"Создана директория {os.path.dirname(target_path)}")
+
+
+def make_original_language_dictionary(original_language_lines: list) -> dict:
+    original_language_dictionary = {}
+    num_str = 0
+    for line in original_language_lines:
+        if line.lstrip() != "":
+            key = line.split()[0]
+            value = line.rstrip()
+        else:
+            key = "transfer"
+            value = "\n"
+        original_language_dictionary[num_str] = {"key": key, "value": value}
+        num_str += 1
+    return original_language_dictionary
 
 
 def main():
@@ -66,69 +81,57 @@ def main():
     previous_translate_path, new_translate_path, need_translate = get_previous_new_path()
     original_hierarchy = get_original_localization_hierarchy(original_path=original_language_path)
     make_new_language_hierarchy(new_translation_path=new_translate_path, original_hierarchy=original_hierarchy,
-                                original_language=original_language, target_language=target_language)
-    if previous_translate_path is not None:
-        with open(file=previous_translate_path, mode="r",
-                  encoding="utf-8-sig") as previous_translate_file, \
-                open(file=original_language_path, mode="r",
-                     encoding="utf-8-sig") as original_language_file, \
-                open(file=new_translate_path, mode="w", encoding="utf-8-sig") as new_translate_file:
+                                original_language=original_language, target_language=target_language)  # Создает иерархию папок, как в оригинальной директории, где потом можно создать объединенные файлы
+    for step in original_hierarchy:
+        full_original_path = os.path.join(original_language_path, step)
+        full_new_path = os.path.join(new_translate_path, step.replace(original_language, target_language))
+        if previous_translate_path is not None:
+            full_previous_path = os.path.join(previous_translate_path, step.replace(original_language, target_language))
+            with open(file=full_previous_path, mode="r",
+                      encoding="utf-8-sig") as previous_translate_file, \
+                    open(file=full_original_path, mode="r",
+                         encoding="utf-8-sig") as original_language_file, \
+                    open(file=full_new_path, mode="w", encoding="utf-8-sig") as new_translate_file:
 
-            original_language_lines = original_language_file.readlines()
-            new_translate_list = ["" for _ in range(len(original_language_lines))]
-            previous_translate_dictionary = {"lang": "l_" + target_language + ":\n"}
-            previous_translate_file.readline()
-            previous_translate_lines = previous_translate_file.readlines()
-            for line in previous_translate_lines:
-                if line.lstrip() != "":
-                    previous_translate_dictionary[line.split()[0]] = line
+                original_language_lines = original_language_file.readlines()
+                new_translate_list = ["" for _ in range(len(original_language_lines))]
+                previous_translate_dictionary = {"lang": "l_" + target_language + ":\n"}
+                previous_translate_file.readline()
+                previous_translate_lines = previous_translate_file.readlines()
+                for line in previous_translate_lines:
+                    if line.lstrip() != "":
+                        previous_translate_dictionary[line.split()[0]] = line
 
-            original_language_dictionary = make_original_language_dictionary(
-                original_language_lines=original_language_lines)
-            for key, values in original_language_dictionary.items():
-                if key == 0:
-                    new_translate_list[0] = previous_translate_dictionary["lang"]
-                else:
-                    response = previous_translate_dictionary.get(values["key"], None)
-                    if response is None and values["key"] != "transfer":
-                        new_translate_list[key] = values["value"] + " #NT!\n"
-                    elif values["key"] == "transfer":
-                        new_translate_list[key] = values["value"]
+                original_language_dictionary = make_original_language_dictionary(
+                    original_language_lines=original_language_lines)
+                for key, values in original_language_dictionary.items():
+                    if key == 0:
+                        new_translate_list[0] = previous_translate_dictionary["lang"]
                     else:
-                        new_translate_list[key] = previous_translate_dictionary[values["key"]]
-            print(*new_translate_list, end="", sep="", file=new_translate_file)
-    else:
-        with open(file=original_language_path, mode="r", encoding="utf-8-sig") as original_language_file, \
-                open(file=new_translate_path, mode="w", encoding="utf-8-sig") as new_translate_file:
-            original_language_lines = original_language_file.readlines()
-            original_language_dictionary = make_original_language_dictionary(
-                original_language_lines=original_language_lines)
-            new_translate_list = ["" for _ in range(len(original_language_lines))]
-            print(original_language_dictionary)
-            for key, values in original_language_dictionary.items():
-                if key == 0:
-                    new_translate_list[0] = "l_" + target_language + ":\n"
-                else:
-                    if values["value"].rstrip() == "":
-                        new_translate_list[key] = values["value"]
-                    else:
-                        new_translate_list[key] = values["value"] + " #NT!\n"
-            print(*new_translate_list, sep="", end="", file=new_translate_file)
-
-
-def make_original_language_dictionary(original_language_lines: list) -> dict:
-    original_language_dictionary = {}
-    num_str = 0
-    for line in original_language_lines:
-        if line.lstrip() != "":
-            key = line.split()[0]
-            value = line.rstrip()
+                        response = previous_translate_dictionary.get(values["key"], None)
+                        if response is None and values["key"] != "transfer":
+                            new_translate_list[key] = values["value"] + " #NT!\n"
+                        elif values["key"] == "transfer":
+                            new_translate_list[key] = values["value"]
+                        else:
+                            new_translate_list[key] = previous_translate_dictionary[values["key"]]
+                print(*new_translate_list, end="", sep="", file=new_translate_file)
         else:
-            key = "transfer"
-            value = "\n"
-        original_language_dictionary[num_str] = {"key": key, "value": value}
-        num_str += 1
-    return original_language_dictionary
+            with open(file=full_original_path, mode="r", encoding="utf-8-sig") as original_language_file, \
+                    open(file=full_new_path, mode="w", encoding="utf-8-sig") as new_translate_file:
+                original_language_lines = original_language_file.readlines()
+                original_language_dictionary = make_original_language_dictionary(
+                    original_language_lines=original_language_lines)
+                new_translate_list = ["" for _ in range(len(original_language_lines))]
+                for key, values in original_language_dictionary.items():
+                    if key == 0:
+                        new_translate_list[0] = "l_" + target_language + ":\n"
+                    else:
+                        if values["value"].rstrip() == "":
+                            new_translate_list[key] = values["value"]
+                        else:
+                            new_translate_list[key] = values["value"] + " #NT!\n"
+                print(*new_translate_list, sep="", end="", file=new_translate_file)
 
 
 if __name__ == "__main__":
