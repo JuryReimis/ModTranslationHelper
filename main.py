1d6d4208bf5e7d8e15745473c5c1352c980121bd
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2022-02-22 11:56:30 +0300

    1) Добавлена проверка, поддерживается ли язык, введенный пользователем. 2) В get_previous_new_path добавлены ожидаемые переменные оригинального языка и языка-цели перевода. 3) В need_translation добавлены ожидаемые переменные оригинального языка и языка-цели перевода. 4) need_translation теперь возвращает None вместо False, при отрицательном ответе пользователя. 5) Создана функция translate_line, получающая класс-Translator и линию, которую необходимо перевести. Возвращает строку текста. 6) Создана функция modify_text. Функция принимает в себя строку текста, паттерн регулярного выражения, по которому необходимо модифицировать строку, флаг, по которому программа ориентируется, какое именно действие нужно выполнить и словарь, который, если передан, несет в себе пары "заменитель-исходный текст". При получении флага modify функция заменяет все слова, удовлетворяющие паттерну на некоторую скрытую переменную, порядковый номер замены. При получении флага return_normal_view, в строке заменяется скрытая переменная на свое значение из словаря. 7) Модуль googletrans заменен на deep_translator. 8) Добавлено информирование пользователя о процессе обработки строк. 9) Добавлено вычисление времени, за которое отрабатывает программа.

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
-import googletrans
+import re
 import os
+import sys
+import time
+from deep_translator import GoogleTranslator
 
 
-def get_previous_new_path() -> [str, str, bool]:
+def get_previous_new_path(original_language: str, target_language: str) -> [str, str, bool]:
     was_previous = input("У вас есть предыдущая версия локализации? y/n ")
     match was_previous.lower():
         case "y":
             previous_translate_path = input("""Введите путь к previous: """)
             new_translate_path = input("""Введите путь к папке new: """)
-
         case "n":
             previous_translate_path = None
             new_translate_path = input("""Введите путь к папке new: """)
         case _:
-            print("Ответ не разборчив, повторите попытку")
-            return get_previous_new_path()
-    need_translate = need_translation()
+            print("Ответ не распознан, повторите попытку")
+            return get_previous_new_path(original_language=original_language, target_language=target_language)
+    need_translate = need_translation(original_language=original_language, target_language=target_language)
     return previous_translate_path, new_translate_path, need_translate
 
 
-def need_translation():
+def need_translation(original_language: str, target_language: str):
     need_translate = input("""Добавить машинный перевод строк? Будет записан рядом с оригинальной в <> y/n """)
     match need_translate.lower():
         case "y":
-            return True
+            return GoogleTranslator(source=original_language, target=target_language)
         case _:
             print(f"Ответ отрицательный или не обработан, вы ввели: {need_translate}, перевод не будет выполнен")
-            return False
+            return None
 
 
 def get_original_localization_hierarchy(original_path: str) -> list:
@@ -62,6 +64,55 @@ def make_original_language_dictionary(original_language_lines: list) -> dict:
     return original_language_dictionary
 
 
+def translate_line(translator: GoogleTranslator | None, line: str) -> str:
+    if translator is None or line.isdigit():
+        return line + " #NT!\n"
+    else:
+        only_text = re.findall(pattern=r"\".+\"", string=line)
+        if not only_text:
+            try:
+                not_content_string = translator.translate(text=line)
+                return not_content_string + "\n"
+            except Exception as e:
+                print("Произошла ошибка с переводом строки:\n", line, "\n", e)
+                return line + " #Translation Error" + "\n"
+        else:
+            try:
+                modified_text, values_dict = modify_text(line=only_text[0], pattern=r"\[.*?\]", flag="modify")
+                if modified_text is None and values_dict is None:
+                    return line + "#NT!\n"
+                translated = translator.translate(text=modified_text[1:-1])
+                normal_string = modify_text(line=translated, pattern=None, flag="return_normal_view",
+                                            values_dict=values_dict)
+                return line + f" <\"{normal_string}\">" + " #NT!\n"
+            except Exception as e:
+                print("Произошла ошибка с переводом строки:\n", line, "\n", e)
+                return line + " #Translation Error!" + "\n"
+
+
+def modify_text(line: str, pattern: str | None, flag: str, values_dict: None | dict = None) -> (str, dict) or str:
+    r"""При флаге "modify" позволяет заменить некоторые части строки по шаблону на скрытую, ничего не обозначающую
+    переменную. При флаге "return_normal_view" позволяет вернуть нормальный вид строки по словарю параметров"""
+    match flag:
+        case "modify":
+            values_dict = {}
+            shadow_number = 0
+            regular_groups = re.findall(pattern=pattern, string=line)
+            for step in regular_groups:
+                values_dict[f"[{shadow_number}]"] = step
+                line = line.replace(step, f"[{shadow_number}]")
+                shadow_number += 1
+            return line, values_dict
+        case "return_normal_view":
+            values_dict: dict
+            for key, value in values_dict.items():
+                line = line.replace(key, value)
+            return line
+        case _:
+            print("Ошибка при модификации, флаг нечитаем")
+            sys.exit()
+
+
 def main():
     print("""Приветствую! Эта программа поможет вам справиться с локализацией многих модов.
     Основной проблемой при локализации является поиск новых строчек для перевода, автор может добавить новые события
@@ -75,13 +126,21 @@ def main():
         В какую папку программа поместит объединенные файлы(имеющие перевод и новые оригинальные) - new""")
     original_language_path = input("Введите путь к папке original. Должна заканчиваться на /localization/[language] ")
     original_language = os.path.basename(original_language_path)
-    print(*googletrans.LANGUAGES.values(), sep="\n")
+    all_languages = GoogleTranslator.get_supported_languages()
+    print(*all_languages, sep="\n")
     target_language = input(f"Выберите язык из списка выше, на который будет производиться локализация ")
 
-    previous_translate_path, new_translate_path, need_translate = get_previous_new_path()
+    if target_language not in all_languages:
+        print(f"Язык {target_language} не поддерживается")
+        sys.exit()
+
+    previous_translate_path, new_translate_path, need_translate = get_previous_new_path(
+        original_language=original_language, target_language=target_language)
     original_hierarchy = get_original_localization_hierarchy(original_path=original_language_path)
     make_new_language_hierarchy(new_translation_path=new_translate_path, original_hierarchy=original_hierarchy,
-                                original_language=original_language, target_language=target_language)  # Создает иерархию папок, как в оригинальной директории, где потом можно создать объединенные файлы
+                                original_language=original_language,
+                                target_language=target_language)  # Создает иерархию папок, как в оригинальной директории, где потом можно создать объединенные файлы
+    start_time = time.time()
     for step in original_hierarchy:
         full_original_path = os.path.join(original_language_path, step)
         full_new_path = os.path.join(new_translate_path, step.replace(original_language, target_language))
@@ -104,13 +163,17 @@ def main():
 
                 original_language_dictionary = make_original_language_dictionary(
                     original_language_lines=original_language_lines)
+                amount_lines = len(original_language_dictionary)
                 for key, values in original_language_dictionary.items():
+                    print(f"Обработка строки №{key}/{amount_lines}",
+                          f"файла {os.path.basename(full_original_path)}")
+                    values: dict
                     if key == 0:
                         new_translate_list[0] = previous_translate_dictionary["lang"]
                     else:
                         response = previous_translate_dictionary.get(values["key"], None)
                         if response is None and values["key"] != "transfer":
-                            new_translate_list[key] = values["value"] + " #NT!\n"
+                            new_translate_list[key] = translate_line(translator=need_translate, line=values["value"])
                         elif values["key"] == "transfer":
                             new_translate_list[key] = values["value"]
                         else:
@@ -123,15 +186,19 @@ def main():
                 original_language_dictionary = make_original_language_dictionary(
                     original_language_lines=original_language_lines)
                 new_translate_list = ["" for _ in range(len(original_language_lines))]
+                amount_lines = len(original_language_dictionary)
                 for key, values in original_language_dictionary.items():
+                    print(f"Обработка строки №{key}/{amount_lines}",
+                          f"файла {os.path.basename(full_original_path)}")
                     if key == 0:
                         new_translate_list[0] = "l_" + target_language + ":\n"
                     else:
                         if values["value"].rstrip() == "":
                             new_translate_list[key] = values["value"]
                         else:
-                            new_translate_list[key] = values["value"] + " #NT!\n"
+                            new_translate_list[key] = translate_line(translator=need_translate, line=values["value"])
                 print(*new_translate_list, sep="", end="", file=new_translate_file)
+    print("Завершено за: ", time.strftime("%H:%M:%S", (time.localtime(time.time() - start_time))))
 
 
 if __name__ == "__main__":
