42bfb9c157f1ba535410e27d299c964dffc8c6a1
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2022-11-24 18:23:41 +0300

    Добавлен метод run, который будет запускать процесс обработки файлов, а также соединена кнопка старта с методом run() Кнопка run_pushButton заблокирована при запуске программы

--- gui.py
+++ gui.py
@@ -12,6 +12,8 @@ class MyWindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
 
         self.last_selected_directory = '/'
+        self.ui.run_pushButton.setText('Start')
+        self.ui.run_pushButton.setEnabled(False)
         for language in GoogleTranslator.get_supported_languages():
             self.ui.selector_original_language_comboBox.addItem(language)
             self.ui.selector_target_language_comboBox.addItem(language)
@@ -22,6 +24,7 @@ class MyWindow(QtWidgets.QMainWindow):
         self.ui.original_directory_pushButton.clicked.connect(self.select_original_directory)
         self.ui.previous_directory_pushButton.clicked.connect(self.select_previous_directory)
         self.ui.target_directory_pushButton.clicked.connect(self.select_target_directory)
+        self.ui.run_pushButton.clicked.connect(self.run)
 
         MyWindow.setFixedSize(self, self.size())
 
@@ -49,9 +52,13 @@ class MyWindow(QtWidgets.QMainWindow):
         self.ui.target_directory_lineEdit.setText(chosen_path)
         self.last_selected_directory = chosen_path
 
+    def run(self):
+        pass
 
-app = QtWidgets.QApplication([])
-application = MyWindow()
-application.show()
 
-sys.exit(app.exec_())
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    application = MyWindow()
+    application.show()
+
+    sys.exit(app.exec_())
