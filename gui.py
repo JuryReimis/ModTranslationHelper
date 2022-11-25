86e461d8f0ca598aee10b75e34c59a3fff116dab
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2022-11-25 18:28:41 +0300

    Прописан метод need_translate_changed(), который улавливает сигнал от чек-бокса и изменяет доступность scrollArea. Изменен метод form_checkbox_cascade(), вместо файлов он теперь получает путь к файлам и вызывает функцию, которая возвращает названия файлов. Чекбоксы все включены по-умолчанию. Прописан метод original_directory_changed(), который запускает создание каскада файлов для scrollArea Прописаны коннекты для чек-бокса и завершения редактирования строки ввода оригинальной директории

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
 from PyQt5 import QtWidgets
 import sys
+
+from main import get_original_localization_hierarchy
 from test1 import Ui_MainWindow
 from deep_translator import GoogleTranslator
 
@@ -24,7 +26,9 @@ class MyWindow(QtWidgets.QMainWindow):
         self.ui.original_directory_pushButton.clicked.connect(self.select_original_directory)
         self.ui.previous_directory_pushButton.clicked.connect(self.select_previous_directory)
         self.ui.target_directory_pushButton.clicked.connect(self.select_target_directory)
+        self.ui.need_translation_checkBox.stateChanged.connect(self.need_translate_changed)
         self.ui.run_pushButton.clicked.connect(self.run)
+        self.ui.original_directory_lineEdit.editingFinished.connect(self.original_directory_changed)
 
         MyWindow.setFixedSize(self, self.size())
 
@@ -39,6 +43,10 @@ class MyWindow(QtWidgets.QMainWindow):
                                                                  directory=self.last_selected_directory)
         self.ui.original_directory_lineEdit.setText(chosen_path)
         self.last_selected_directory = chosen_path
+        self.original_directory_changed()
+
+    def original_directory_changed(self):
+        self.form_checkbox_cascade(self.ui.original_directory_lineEdit.text())
 
     def select_previous_directory(self):
         chosen_path = QtWidgets.QFileDialog.getExistingDirectory(caption='Get Path',
@@ -52,14 +60,22 @@ class MyWindow(QtWidgets.QMainWindow):
         self.ui.target_directory_lineEdit.setText(chosen_path)
         self.last_selected_directory = chosen_path
 
-    def form_checkbox_cascade(self, files: list):
+    def form_checkbox_cascade(self, path: str):
+        files = get_original_localization_hierarchy(path)
         vertical_layout_widget = QtWidgets.QWidget()
         vertical_layout = QtWidgets.QVBoxLayout(vertical_layout_widget)
         for file_name in files:
             check_box = QtWidgets.QCheckBox(file_name)
+            check_box.setChecked(True)
             vertical_layout.addWidget(check_box)
         vertical_layout_widget.setLayout(vertical_layout)
         self.ui.need_translate_scrollArea.setWidget(vertical_layout_widget)
+
+    def need_translate_changed(self):
+        if self.ui.need_translation_checkBox.isChecked():
+            self.ui.need_translate_scrollArea.setEnabled(True)
+        else:
+            self.ui.need_translate_scrollArea.setEnabled(False)
 
     def run(self):
         pass
