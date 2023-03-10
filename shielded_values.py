4d6be90ba399b9ebb02ee07c37dc864386a5fc09
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2023-03-10 15:07:21 +0300

    Создан класс для хранения списка паттернов поиска экранируемых частей строк

---
+++ shielded_values.py
@@ -0,0 +1,15 @@
+
+
+class ShieldedValues:
+    wrapped = [
+        r"\[.*?\]",
+        r"§\S*?§",
+        r"\$\S*?\$",
+        r"£\S*?£",
+        r"£\S*",
+
+    ]
+
+    @classmethod
+    def get_common_pattern(cls):
+        return '|'.join(cls.wrapped)
