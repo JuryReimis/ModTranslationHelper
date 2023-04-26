c991f17a79f1f37170026dd615649eb756116fce
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2023-04-26 19:20:40 +0300

    Создан файл с настройками.

---
+++ settings.py
@@ -0,0 +1,10 @@
+# Файл с общими настройками проекта
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+TRANSLATIONS_DIR = BASE_DIR / 'languages'
+HOME_DIR = Path.home()
+SCREEN_SIZE = None
+
+PROGRAM_VERSION = '1.4.0 beta'
+
