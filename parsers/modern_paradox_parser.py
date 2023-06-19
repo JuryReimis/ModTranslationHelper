39c61b5c3e96a13760e5b0890ecc2748c3f4fec3
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2023-06-19 15:36:04 +0300

    Изменен метод парсинга файла. Исправлены ошибки, связанные с неправильным подсчетом количества строк из-за взаимной замены ключей в словаре. Исправлено регулярное выражение для возможности захвата пустых строк локализации.

--- parsers/modern_paradox_parser.py
+++ parsers/modern_paradox_parser.py
@@ -1,5 +1,4 @@
 import re
-from collections import OrderedDict
 from pathlib import Path
 
 from loguru import logger
@@ -10,16 +9,22 @@ class ModernParadoxParser:
         self._filename = filename
 
     @logger.catch()
-    def parse_file(self) -> OrderedDict:
-        final_dictionary = OrderedDict()
+    def parse_file(self, get_list: bool = False) -> dict | list:
+        if get_list:
+            result_storage = []
+        else:
+            result_storage = {}
         if self._filename.is_file() and self._filename.suffix in ['.yml', '.txt', ]:
             with self._filename.open(mode='r', encoding='utf-8-sig') as file:
                 lines = file.readlines()
                 for line in lines:
                     key = self._get_localization_key(line=line)
                     value = self._get_localization_value(line=line)
-                    final_dictionary[key] = value
-        return final_dictionary
+                    if get_list:
+                        result_storage.append({'key': key, 'value': value})
+                    else:
+                        result_storage[key] = value
+        return result_storage
 
     @staticmethod
     def _get_localization_key(pattern=r"(.*:)(\d*)( *)(\".*\")", line='') -> str | None:
@@ -30,7 +35,7 @@ class ModernParadoxParser:
             return line.rstrip()
 
     @staticmethod
-    def _get_localization_value(pattern: str = r'(\".*\w+?.*\")', line: str = '') -> str | None:
+    def _get_localization_value(pattern: str = r'(\".*\w*?.*\")', line: str = '') -> str | None:
         value = re.findall(pattern=pattern, string=line)
         if value:
             return value[0].rstrip()
