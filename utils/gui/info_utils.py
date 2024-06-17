7bc28cf936ef575891b5fd3aed756b5d20ea462b
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2024-06-17 19:16:31 +0300

    Класс для добавления иконки с подсказкой перенесен в отдельный каталог

---
+++ utils/gui/info_utils.py
@@ -0,0 +1,31 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import QSize
+from PyQt5.QtWidgets import QLabel
+
+import qtawesome as qta
+
+
+class AddInfoIcons:
+    stylesheet = "QToolTip { color: #29ab87;" \
+                 " font-size: 18px;" \
+                 " font-weight: bold; }"
+
+    def __init__(self, layouts: dict):
+        for layout, text in layouts.items():
+            layout: QtWidgets.QHBoxLayout
+            info_icon = layout.itemAt(1)
+            if not info_icon:
+                info_icon = self.get_icon()
+                info_icon.setToolTip(text)
+                info_icon.setStyleSheet(self.stylesheet)
+                layout.addWidget(info_icon)
+
+            else:
+                info_icon.widget().setStyleSheet(self.stylesheet)
+                info_icon.widget().setToolTip(text)
+
+    @staticmethod
+    def get_icon():
+        icon = QLabel()
+        icon.setPixmap(qta.icon('fa5.question-circle').pixmap(QSize(16, 16)))
+        return icon
